481bfa34628ef94a249b74c47a14c23951cc8c8d
Ken Yamada <yamakenth@gmail.com>
2022-02-16 19:44:57 +0700

    Create sign up form with ability to post data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+const { body, validationResult } = require('express-validator');
+
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/sign-up', function(req, res, next) {
-  res.send('NOT IMPLEMENTED: Sign-up Form');
+  res.render('sign-up-form');
 });
+
+router.post('/sign-up', [
+  body('first_name').trim().escape(),
+  body('last_name').trim().escape(),
+  body('email').isEmail().normalizeEmail(),
+  body('password').escape(),
+  body('password-confirmation')
+    .trim()
+    .escape()
+    .custom(function(value, { req }) {
+      if (value !== req.body.password) {
+        throw new Error('Password confirmation does not match password');
+      }
+      return true;
+    }),
+
+  function(req, res, next) {
+    var errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('sign-up-form', { user: req.body, errors: errors.array() });
+      return;
+    } 
+
+    var user = new User({
+      first_name: req.body.first_name.toUpperCase(),
+      last_name: req.body.last_name.toUpperCase(),
+      username: req.body.email.toLowerCase(),
+      password: req.body.password
+    });
+    user.save(function(err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
+  }
+]
+);
 
 module.exports = router;
